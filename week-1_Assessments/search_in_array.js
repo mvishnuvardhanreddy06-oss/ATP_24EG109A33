f97f4bb35bb333d19dde01277750bbe8679b0afa
mvishnuvardhanreddy06-oss <mvishnuvardhanreddy06@gmail.com>
2026-05-19 14:57:31 +0530

    Update search_in_array.js

--- week-1_Assessments/search_in_array.js
+++ week-1_Assessments/search_in_array.js
@@ -1,8 +1,5 @@
 /**
  * Function to search for an element in an array.
- * @param {Array} arr The array to search in.
- * @param {*} searchElement The element to look for.
- * @returns {number|string} The index of the element or "not found".
  */
 const searchNum = (arr, searchElement) => {
   const index = arr.indexOf(searchElement);
